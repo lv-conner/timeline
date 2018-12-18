e9a7aa0fb324a0d272831202b3647d049d6b73da
lv-conner <lvmh123456@gmail.com>
2018-12-18 09:21:52 +0800

    timeline 奇偶数css选择器

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Table } from "antd";
+import "antd/dist/antd.css";
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,20 +8,47 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <div className="timeline-container">
+              <div className="timeline-liner">
+                 <div className="time-line-content">
+                     <span></span>
+                     <div className="time-line-box">
+                        Content
+                     </div>
+                 </div>
+                 <div className="time-line-content">
+                     <span></span>
+                     <div className="time-line-box">
+                        Content
+                     </div>
+                 </div>
+                 <div className="time-line-content">
+                     <span></span>
+                     <div className="time-line-box">
+                        Content
+                     </div>
+                 </div>
+                 <div className="time-line-content">
+                     <span></span>
+                     <div className="time-line-box">
+                        Content
+                     </div>
+                 </div>
+                 <div className="time-line-content">
+                     <span></span>
+                     <div className="time-line-box">
+                        Content
+                     </div>
+                 </div>
+                 <div className="time-line-content">
+                     <span></span>
+                     <div className="time-line-box">
+                        Content
+                     </div>
+                 </div>
+              </div>
+          </div>
+
       </div>
     );
   }
